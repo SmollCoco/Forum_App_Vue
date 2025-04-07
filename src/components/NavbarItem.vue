9c6f512d45a47fa884a06beed8c6e391c9a62233
ayaigadern <ayaigadern>
2025-04-07 23:20:41 +0100

    Created Navbar, Added Necessary Routes, firebase file added

---
+++ src/components/NavbarItem.vue
@@ -0,0 +1,30 @@
+<script setup>
+const props=defineProps({
+  username:{
+    type: String,
+    default: "",
+  }
+});
+
+
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <router-link to="/" class="navbar-brand h1 fs-1" >Forum</router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
+      <div v-if="username">
+        <button class="btn btn-info"><router-link :to="`/profile/${username}`" class="nav-link active" aria-current="page" >Profile</router-link></button>
+      </div>
+      <div v-else class="d-flex gap-3">
+        <button class="btn btn-outline-primary"><router-link to="/login" class="nav-link active" aria-current="page" >Login</router-link></button>
+        <button class="btn btn-outline-success"><router-link to="/register" class="nav-link active" aria-current="page" >Register</router-link></button>
+      </div>
+    </div>
+  </div>
+</nav>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import LoginView from "../views/LoginView.vue";
@@ -5,11 +6,21 @@ import RegisterView from "../views/RegisterView.vue";
 import AdminView from "../views/AdminView.vue";
 import DiscussionView from "../views/DiscussionView.vue";
 import ProfileView from "../views/ProfileView.vue";
+=======
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
+import AdminView from '../views/AdminView.vue'
+import DiscussionView from '../views/DiscussionView.vue'
+import ProfileView from '../views/ProfileView.vue'
+>>>>>>> e8d23f6 (Created Navbar, Added Necessary Routes, firebase file added)
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+<<<<<<< HEAD
       path: "/",
       name: "Home",
       component: HomeView,
@@ -38,7 +49,37 @@ const router = createRouter({
       path: "/Admin/:username",
       name: "Admin",
       component: AdminView,
+=======
+      path: '/',
+      name: 'Home',
+      component: HomeView
     },
+    {
+      path: '/login',
+      name: 'Login',
+      component: LoginView
+>>>>>>> e8d23f6 (Created Navbar, Added Necessary Routes, firebase file added)
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: RegisterView
+    },
+    {
+      path: '/profile/:username',
+      name: 'Profile',
+      component: ProfileView
+    },
+    {
+      path: '/discussion/:id',
+      name: 'Discussion',
+      component: DiscussionView,
+    },
+    {
+      path: '/Admin/:username',
+      name: 'Admin',
+      component: AdminView,
+    }
   ],
 });
 
