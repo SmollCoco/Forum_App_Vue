3c14dc6d15b01ce83214be56ae5d9b19422410cb
ibhiyassine <ibhiyassine@gmail.com>
2025-04-07 21:54:00 +0100

    Fixed Naming of Files

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,45 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
+import RegisterView from "../views/RegisterView.vue";
+import AdminView from "../views/AdminView.vue";
+import DiscussionView from "../views/DiscussionView.vue";
+import ProfileView from "../views/ProfileView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "Home",
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: "/login",
+      name: "Login",
+      component: LoginView,
+    },
+    {
+      path: "/register",
+      name: "Register",
+      component: RegisterView,
+    },
+    {
+      path: "/Profile",
+      name: "Profile",
+      component: ProfileView,
+    },
+    {
+      path: "/discussion/:username",
+      name: "Discussion",
+      component: DiscussionView,
+    },
+    {
+      path: "/Admin/:username",
+      name: "Admin",
+      component: AdminView,
     },
   ],
-})
+});
 
-export default router
+export default router;
