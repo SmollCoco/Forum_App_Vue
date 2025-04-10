d29e151a5480663c98f2eb580a6274dab279a017
ayaigadern <ayaigader@gmail.com>
2025-04-10 15:08:19 +0100

    Added ability to fetch

---
+++ src/components/DiscussionList.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import DiscussionItem from './DiscussionItem.vue'
+import { useStore } from '@/composables/getDiscussions';
+
+const { discussions, fetchDiscussions } = useStore();
+onMounted(() => {
+  fetchDiscussions().then(() => {
+    
+    console.log('discussions1', discussions.value);
+  });
+});
+</script>
+
+<template>
+    <div class="d-flex justify-content-center">
+        <div class="w-75">
+            <hr class="border-1 m-0" />
+            <div v-for="discussion in discussions" :key="discussion.id">
+                <div v-if="discussion.parent==null">
+                    <DiscussionItem v-bind="discussion" />
+                    <hr class="border-1 m-0" />
+
+                </div>
+        
+      </div>
+            
+        </div>
+    </div>
+</template>
--- src/components/SearchSuggestionItem.vue
+++ src/components/SearchSuggestionItem.vue
@@ -1,11 +1,39 @@
+<!-- eslint-disable no-unused-vars -->
 <script setup>
+    const props = defineProps({
+        type: {
+            type: String,
+            required: true,
+            default: "User", // Can be either a user or a 'Topic'
+        },
+        value: {
+            type: String,
+            required: true,
+            default: "Aya",
+        }
+    })
 </script>
 
 <template>
-    <div>
-
+    <div class="w-100 p-2 d-flex gap-lg-1 border-bottom">
+        <router-link v-if="type == 'User' " :to="`/profile/${value}`"
+            class="d-flex gap-4 text-decoration-none align-items-center"><img src="../assets/user.png" width="40"
+                class="rounded-circle d-block"> <span class="d-block link"> u/{{ value }} </span></router-link>
+                <!--else if type == 'Topic' -->
+        <router-link v-else :to="`/discussion/topic/${value}`"
+            class="d-flex gap-4 text-decoration-none align-items-center"><img src="../assets/tag.png" width="40"
+                class="rounded-circle d-block"> <span class="d-block link"> topic/{{ value }} </span></router-link>
     </div>
+    
 </template>
 
 <style scoped>
+.link{
+    font-weight: bold;
+    color: black;
+    text-decoration: none;
+}
+.link:hover{
+    color: rgb(10, 73, 209);
+}
 </style>
