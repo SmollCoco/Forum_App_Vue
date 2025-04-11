148e2254d51bd7735e3873d2ccda7492e12fe92d
ibhiyassine <ibhiyassine@gmail.com>
2025-04-11 09:48:52 +0100

    Finalized the Discussion Card, logic is missing tho

--- src/components/DiscussionItem.vue
+++ src/components/DiscussionItem.vue
@@ -1,7 +1,8 @@
 <!-- eslint-disable no-unused-vars -->
 <script setup>
-import { computed } from 'vue';
+import { ref, computed } from 'vue';
 import TopicItem from './TopicItem.vue';
+import ReplyModal from './ReplyModal.vue';
 const props = defineProps({
     id: {
         type: String,
@@ -27,51 +28,67 @@ const props = defineProps({
         type: Array,
         required: true,
         default: () => ["Friendship", "Leadership"],
+    },
+    contenu: {
+        type: String,
+        required: true,
+        default: "Some content",
     }
 });
 
 let date_string = computed(() => {
-  const timestamp = props.date;
+    const timestamp = props.date;
 
-  if (timestamp && typeof timestamp.seconds === 'number') {
-    const jsDate = new Date(timestamp.seconds * 1000);
+    if (timestamp && typeof timestamp.seconds === 'number') {
+        const jsDate = new Date(timestamp.seconds * 1000);
 
-    return jsDate.toLocaleString('fr-FR', {
-      weekday: 'long',     // optionnel : "jeudi"
-      day: '2-digit',      // "10"
-      month: 'long',       // "avril"
-      year: 'numeric',     // "2025"
-      hour: '2-digit',     // "14"
-      minute: '2-digit',   // "05"
-      hour12: false        // format 24h
-    });
-  }
+        return jsDate.toLocaleString('fr-FR', {
+            weekday: 'long',     // optionnel : "jeudi"
+            day: '2-digit',      // "10"
+            month: 'long',       // "avril"
+            year: 'numeric',     // "2025"
+            hour: '2-digit',     // "14"
+            minute: '2-digit',   // "05"
+            hour12: false        // format 24h
+        });
+    }
 
-  return "Date invalide";
+    return "Date invalide";
 });
 
+let preview = computed(() => {
+    return props.contenu.substring(0, 50);
+});
 
+let show_response = ref(false);
 </script>
 
 <template>
-    <router-link :to="`/discussion/${id}`" class="r-link mx-1">
-        <div class="w-100 d-flex gap-lg-2 flex-column rounded p-2 r-link">
-            <!--Header containing the author, the topics and the date-->
-            <div class="d-flex align-items-center gap-lg-2 z-1">
-                <router-link :to="`/profile/${auteur}`" class="d-flex gap-2 text-decoration-none align-items-center">
-                    <img src="../assets/discussion.png" width="40" class="rounded-circle d-block">
-                    <span class="d-block link div-link"> u/{{ auteur }}
-                    </span>
-                </router-link>
-                <span style="color: gray; font-size: small;"> | {{ date_string }} | </span>
-                <div v-for="(topic, index) of topic" :key="index">
-                    <topic-item :topic="topic" />
+    <div class="m-2 p-2 r-link">
+        <router-link :to="`/discussion/${id}`" class="r-link">
+            <div class="w-100 d-flex gap-lg-2 flex-column rounded p-2">
+                <!--Header containing the author, the topics and the date-->
+                <div class="d-flex align-items-center gap-lg-2 z-1">
+                    <router-link :to="`/profile/${auteur}`"
+                        class="d-flex gap-2 text-decoration-none align-items-center">
+                        <img src="../assets/discussion.png" width="40" class="rounded-circle d-block">
+                        <span class="d-block link div-link"> u/{{ auteur }}
+                        </span>
+                    </router-link>
+                    <span style="color: gray; font-size: small;"> | {{ date_string }} | </span>
+                    <div v-for="(topic, index) of topic" :key="index">
+                        <topic-item :topic="topic" />
+                    </div>
                 </div>
+                <div class="fs-3 fw-bold div-link"> {{ titre }}</div>
+                <div> {{ preview }} </div>
             </div>
-            <div class="fs-3 fw-bold div-link"> {{ titre }}</div>
-            <div id="reply button">tres bien </div>
+        </router-link>
+        <div class="btn rounded-pill fw-bold fx-w" @click="show_response = !show_response">
+            Reply
         </div>
-    </router-link>
+        <reply-modal v-if="show_response" :to_whom="auteur" :parent_id="id" @cancel="show_response = false" />
+    </div>
 </template>
 
 <style scoped>
@@ -94,7 +111,17 @@ let date_string = computed(() => {
 .r-link:hover {
     background-color: rgba(0, 0, 0, 0.05);
 }
-.div-link:hover{
+
+.div-link:hover {
     text-decoration: underline;
+}
+.fx-w{
+    width: 75px;
+    position: relative;
+    background-color: rgb(219, 219, 219);
+}
+.fx-w:hover{
+    background-color: black;
+    color: white;
 }
 </style>
--- src/components/NavbarItem.vue
+++ src/components/NavbarItem.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable no-unused-vars -->
 <script setup>
 import { ref, onMounted } from 'vue';
 // import { useStore } from '@/composables/getDiscussions';
