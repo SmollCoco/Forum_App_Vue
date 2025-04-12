28dcfae1bb18485b333fdf874bf37b20c7b009ec
ayaigadern <ayaigader@gmail.com>
2025-04-12 01:58:30 +0100

    date filter added

--- src/components/DiscussionList.vue
+++ src/components/DiscussionList.vue
@@ -3,6 +3,7 @@ import { ref, onMounted, watch } from 'vue';
 import DiscussionItem from './DiscussionItem.vue';
 import { useStore } from '@/composables/getDiscussions';
 import { getTopic_Disc } from '@/composables/getTopic_Disc';
+import {DateFilter} from '@/composables/date_filter';
 
 const props = defineProps({
   topic: {
@@ -19,7 +20,7 @@ async function filterDiscussions() {
 
   await fetchDiscussions();
 
-
+console.log(discussions.value[0].date);
   if (!props.topic) {
     filteredDiscussions.value = discussions.value.filter(disc => disc.parent == null);
 
@@ -33,10 +34,19 @@ async function filterDiscussions() {
 watch(() => props.topic, filterDiscussions);
 
 onMounted(filterDiscussions);
+
+const ordre=ref("Date ⬇");
+ function toogle(){
+  ordre.value=ordre.value=="Date ⬆"? ordre.value="Date ⬇" : ordre.value="Date ⬆";
+  filteredDiscussions.value=DateFilter(filteredDiscussions.value,ordre.value);
+
+}
 </script>
 
 <template>
+  <div type="button" class="btn rounded-pill fw-bold fx-w mx-5 my-2" v-on:click="toogle">{{ ordre }}</div>
   <div class="d-flex justify-content-center">
+    
     <div class="w-75">
       <hr class="border-1 m-0" />
       <div v-for="discussion of filteredDiscussions" :key="discussion.id">
@@ -50,3 +60,16 @@ onMounted(filterDiscussions);
     </div>
   </div>
 </template>
+<style scoped>
+.fx-w {
+    width: 90px;
+    position: relative;
+    background-color: rgb(219, 219, 219);
+    align-self:end ;
+}
+
+.fx-w:hover {
+    background-color: black;
+    color: white;
+}
+</style>
